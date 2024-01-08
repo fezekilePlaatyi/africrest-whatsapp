105654d9272635f158257442722635fe4df665ec
Fezekile Plaatyi <fezekileplaatyi@myplusplus.com>
2024-01-08 23:54:17 +0200

    Enhanced functionality:

--- chatbot/handler.js
+++ chatbot/handler.js
@@ -1,7 +1,68 @@
-const { sendWelcomeTemplate } = require("./whatsapp_processor");
+const {
+  sendWelcomeTemplate,
+  sendMessageByTemplateId,
+  sendAccessCodeTemplate,
+  sendDyamicMessage,
+} = require("./whatsapp_processor");
+const moment = require("moment");
 
 const verifyToken = (req) => {
   return req.query["hub.verify_token"] === "helloworld";
+};
+
+let users = [
+  {
+    emailAddress: "fezekileplaatyi@gmail.com",
+    cellNumber: "+27780687445",
+    whatsAppNumber: "+27780687445",
+    name: "Fezekile",
+    surname: "Plaatyi",
+    roomNumber: "3409",
+    activeSessionDetails: {
+      currentStatus: 0,
+    },
+  },
+];
+
+const generateAccessCode = async () => {
+  // Generate a random 5-digit number
+  //   const randomNumber = Math.floor(10000 + Math.random() * 90000);
+
+  //   // Ensure the generated number is exactly 5 digits
+  //   const accessCode = randomNumber.toString().padStart(5, "0");
+
+  return Math.floor(Math.random() * 90000) + 10000;
+};
+
+const updateStatus = async (whatsAppId, status) => {
+  const formattedCellNumber = `+${whatsAppId.replace(/\D/g, "")}`;
+
+  users.forEach((user) => {
+    if (
+      user.whatsAppNumber === formattedCellNumber ||
+      user.cellNumber === formattedCellNumber
+    ) {
+      user.activeSessionDetails.currentStatus = status;
+    }
+  });
+  return users;
+};
+
+const checkUserExistence = (cellNumber) => {
+  const formattedCellNumber = `+${cellNumber.replace(/\D/g, "")}`;
+
+  const foundUser = users.find(
+    (user) =>
+      user.cellNumber === formattedCellNumber ||
+      user.whatsAppNumber === formattedCellNumber
+  );
+
+  const result = {
+    exist: foundUser !== undefined,
+    details: foundUser || null,
+  };
+
+  return result;
 };
 
 const handler = async (req, res) => {
@@ -20,11 +81,58 @@ const handler = async (req, res) => {
     if (!!whatsAppPayload.contacts) {
       const whatsAppId = whatsAppPayload.contacts[0].wa_id;
       const typeOfMessage = whatsAppPayload.messages[0].type;
+      const result = checkUserExistence(whatsAppId);
 
       if (typeOfMessage === "text") {
         const messageBody = whatsAppPayload.messages[0].text.body;
-        await sendWelcomeTemplate(whatsAppId);
-        res.send("ok");
+
+        if (result.exist) {
+          if (result.details.activeSessionDetails.currentStatus == 0) {
+            await sendWelcomeTemplate(whatsAppId);
+            users = await updateStatus(whatsAppId, 1);
+            res.send("ok");
+          } else if (result.details.activeSessionDetails.currentStatus == 1) {
+            if (messageBody.trim() == "1") {
+              await sendMessageByTemplateId(
+                whatsAppId,
+                "the_apollo_access_code_main_menu_temp"
+              );
+              users = await updateStatus(whatsAppId, 2);
+              res.send("ok");
+            }
+          }
+        } else {
+          await sendWelcomeTemplate(whatsAppId);
+          res.send("ok");
+        }
+      } else if (typeOfMessage === "button") {
+        if (result.exist) {
+          if (result.details.activeSessionDetails.currentStatus == 0) {
+            users = await updateStatus(whatsAppId, 0);
+            await sendMessageByTemplateId(
+              whatsAppId,
+              "the_apollo_invalid_input_temp"
+            );
+            await sendWelcomeTemplate(whatsAppId);
+          } else if (result.details.activeSessionDetails.currentStatus == 2) {
+            users = await updateStatus(whatsAppId, 0);
+
+            const today = moment().endOf("day");
+            const expiryDateTime = today.format("YYYY-MM-DD HH:mm:ss");
+            const accessCode = await generateAccessCode();
+            const use =
+              whatsAppPayload.messages[0].button.payload == "Tap2 - 2x Entry"
+                ? "2"
+                : "1";
+
+            const messageBody = `*🔐The Apollo Tap Access Code*\n\nThe access code for your visitor is *${accessCode}*.\n\nThis code is valid for *${use} use${
+              use > 1 ? "s" : ""
+            }* until *${expiryDateTime}*.\n \n *Instructions:*\n Please provide this code to the security personnel at the gate for your visitor's access. The code allows two entries.\n \n If your visitor encounters any issues or if you have questions, please let us know. Thank you! `;
+
+            await sendDyamicMessage(whatsAppId, messageBody);
+            res.send("ok");
+          }
+        }
       }
     }
   }
--- chatbot/whatsapp_processor.js
+++ chatbot/whatsapp_processor.js
@@ -18,7 +18,78 @@ const sendRestApi = async (payload) => {
   });
 };
 
-const sendWelcomeTemplate = (whatsAppId) => {
+const sendMessageByTemplateId = async (whatsAppId, templateId) => {
+  const payload = {
+    messaging_product: "whatsapp",
+    to: whatsAppId,
+    type: "template",
+    template: {
+      name: templateId,
+      language: {
+        code: "en_US",
+      },
+    },
+  };
+
+  return sendRestApi(payload);
+};
+
+const sendAccessCodeTemplate = async (
+  whatsAppId,
+  accessCode,
+  use,
+  expiryDateTime
+) => {
+  const payload = {
+    messaging_product: "whatsapp",
+    to: whatsAppId,
+    type: "template",
+    template: {
+      name: "the_apollo_access_code_response_temp",
+      language: {
+        code: "en_US",
+      },
+      components: [
+        {
+          type: "body",
+          parameters: [
+            {
+              type: "text",
+              text: accessCode,
+            },
+            {
+              type: "text",
+              text: use,
+            },
+            {
+              type: "text",
+              text: expiryDateTime,
+            },
+          ],
+        },
+      ],
+    },
+  };
+
+  return sendRestApi(payload);
+};
+
+const sendDyamicMessage = (whatsAppId, messageBody) => {
+  const payload = {
+    messaging_product: "whatsapp",
+    recipient_type: "individual",
+    to: whatsAppId,
+    type: "text",
+    text: {
+      preview_url: false,
+      body: messageBody,
+    },
+  };
+
+  return sendRestApi(payload);
+};
+
+const sendWelcomeTemplate = async (whatsAppId) => {
   const payload = {
     messaging_product: "whatsapp",
     to: whatsAppId,
@@ -47,9 +118,14 @@ const sendWelcomeTemplate = (whatsAppId) => {
   return sendRestApi(payload);
 };
 
-const sendWelcomeTemplateNonExistingNumber = async () => {};
+const sendWelcomeTemplateNonExistingNumber = async () => {
+  return sendRestApi(payload);
+};
 
 module.exports = {
   sendWelcomeTemplate: sendWelcomeTemplate,
   sendWelcomeTemplateNonExistingNumber: sendWelcomeTemplateNonExistingNumber,
+  sendMessageByTemplateId: sendMessageByTemplateId,
+  sendAccessCodeTemplate: sendAccessCodeTemplate,
+  sendDyamicMessage: sendDyamicMessage,
 };
